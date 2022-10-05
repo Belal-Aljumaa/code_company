9b8ed8c5c409d009eaf14b5b5251962f8a40d9fa
Belal-Aljumaa <bilal.joma90@gmail.com>
2022-10-05 23:20:14 +0200

    new sections added and edited

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,26 @@
-const gulp = require('gulp');  // to import gulp from gulp modules
-const concat = require('gulp-concat');  // to concat files
-const autoprefixer = require('gulp-autoprefixer');  // to add css prefixes
-const sass = require('gulp-sass')(require('sass'));  // to manipulate sass file
-const pug = require('gulp-pug');  // for html pug
-const livereload = require('gulp-livereload');  // for livereload
-const sourcemaps = require('gulp-sourcemaps');  // to create a map
-const uglify = require('gulp-uglify');  // to minify js scripts
-const notify = require('gulp-notify');  // to show notifications
-const zip = require('gulp-zip');  // to compress files
+const gulp = require("gulp"); // to import gulp from gulp modules
+const concat = require("gulp-concat"); // to concat files
+const autoprefixer = require("gulp-autoprefixer"); // to add css prefixes
+const sass = require("gulp-sass")(require("sass")); // to manipulate sass file
+const pug = require("gulp-pug"); // for html pug
+const livereload = require("gulp-livereload"); // for livereload
+const sourcemaps = require("gulp-sourcemaps"); // to create a map
+const uglify = require("gulp-uglify"); // to minify js scripts
+const notify = require("gulp-notify"); // to show notifications
+const zip = require("gulp-zip"); // to compress files
+const imagemin = require("gulp-imagemin"); // to compress images
+const webp = require("imagemin-webp");
 
+// images Task compress
+gulp.task("images", function () {
+  return gulp
+    .src("src/images/*")
+    .pipe(imagemin([webp({ quality: 25 })]))
+    .pipe(gulp.dest("dist/images"));
+});
 
 // Html Task
-gulp.task('html', function () {
+gulp.task("html", function () {
   return gulp
     .src("src/assets/index.pug")
     .pipe(pug({ pretty: true }))
@@ -21,23 +30,21 @@ gulp.task('html', function () {
 });
 
 // Css Task
-gulp.task('css', function () {
-  return (
-    gulp
-      .src("src/assets/css/main.scss")
-      .pipe(sourcemaps.init())
-      .pipe(sass())
-      .pipe(autoprefixer("last 2 versions"))
-      .pipe(concat("main.css"))
-      .pipe(sourcemaps.write("."))
-      .pipe(gulp.dest("dist/css"))
-      .pipe(notify("CSS Task Is Done"))
-      .pipe(livereload())
-  );
+gulp.task("css", function () {
+  return gulp
+    .src("src/assets/css/main.scss")
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(autoprefixer("last 2 versions"))
+    .pipe(concat("main.css"))
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest("dist/css"))
+    .pipe(notify("CSS Task Is Done"))
+    .pipe(livereload());
 });
 
 // Js Task
-gulp.task('js', function () {
+gulp.task("js", function () {
   return gulp
     .src("src/assets/js/*.js")
     .pipe(concat("main.js"))
@@ -48,26 +55,27 @@ gulp.task('js', function () {
 });
 
 // Compress Files
-gulp.task('compress', function () {
+gulp.task("compress", function () {
   return gulp
-    .src('dist/**/*.*')
-    .pipe(zip('website.zip'))
-    .pipe(gulp.dest('.'))
-    .pipe(notify('Files Compressed'));
+    .src("dist/**/*.*")
+    .pipe(zip("website.zip"))
+    .pipe(gulp.dest("."))
+    .pipe(notify("Files Compressed"));
 });
 
 // Watch Task
-gulp.task('watch', function () {
-  require('./server.js');
+gulp.task("watch", function () {
+  require("./server.js");
   livereload.listen();
+  gulp.watch("src/images/*", gulp.series("images"));
   gulp.watch(
     ["src/assets/index.pug", "src/assets/pug/*.pug"],
     gulp.series("html")
   );
   gulp.watch("src/assets/css/**/*.scss", gulp.series("css"));
-  gulp.watch('src/assets/js/*.js', gulp.series('js'));
-  gulp.watch('dist/**/*.*', gulp.series('compress'));
+  gulp.watch("src/assets/js/*.js", gulp.series("js"));
+  gulp.watch("dist/**/*.*", gulp.series("compress"));
 });
 
 // Default Task
-gulp.task('default', gulp.series('watch'));
+gulp.task("default", gulp.series("watch"));
